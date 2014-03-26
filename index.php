512a5c75c0e5784d01a9e99536ba878eade8d435
glenn <glennholt1@gmail.com>
2014-03-26 05:30:40 -0700

    added db connection

--- index.php
+++ index.php
@@ -3,6 +3,8 @@
 # the return value is a string that will work with pg_connect
 function pg_connection_string() {
   // we will fill this out next
+  return "dbname=ddqshbbp5hf289 host=ec2-54-197-246-197.compute-1.amazonaws.com port=5432 user=aukxnojvkjwmqm password=ytJA96yoCwF38KHi9W0fcC1NVU sslmode=require"
+
 }
  
 # Establish db connection
